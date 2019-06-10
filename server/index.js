2becb690160a3c5e653490832feeb92ccad266db
Pedro Gomez <makhnnar@gmail.com>
2019-06-09 22:23:25 -0400

    init project

---
+++ server/index.js
@@ -0,0 +1,13 @@
+/**
+*	Para este proyecto, usaremos la posibilidad que nos ofrece 
+* 	express de crear una mejor estructuracion del proyecto usando rutas.
+*	Por ello de forma completamente intencional ya agrupe el minimo de
+*	carpetas y archivos que estaras manejando, van a ser muchos mas, pero
+*	por lo menos con esto ya tienen una base para mantener la arquitectura
+*	lo que menciono lo pueden buscar en la web como
+*	
+*	var express = require('express');
+*	var router = express.Router();
+*
+*	para mas informacion visiten la documentacion oficial. Esto es obligatorio
+*/
